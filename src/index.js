a0f759b9f422055ff0f90ee41609715fa6163fdc
Shannqa <shannqa@gmail.com>
2023-10-08 22:19:11 +0200

    Work on custom form validation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import css from "./style.css";
 
+// prepare a form
+
 const body = document.querySelector("body");
 const form = document.createElement("form");
 const inputEmail = document.createElement("input");
@@ -13,10 +15,16 @@ const labelCountry = document.createElement("label");
 const labelZip = document.createElement("label");
 const labelPass = document.createElement("label");
 const labelPassConf = document.createElement("label");
+const spanEmail = document.createElement("span");
+const spanCountry = document.createElement("span");
+const spanZip = document.createElement("span");
+const spanPass = document.createElement("span");
+const spanPassConf = document.createElement("span");
 
 labelEmail.setAttribute("for", "email");
 inputEmail.setAttribute("id", "email");
 inputEmail.setAttribute("name", "email");
+inputEmail.setAttribute("type", "email");
 labelCountry.setAttribute("for", "country");
 inputCountry.setAttribute("id", "country");
 inputCountry.setAttribute("name", "country");
@@ -39,13 +47,32 @@ button.textContent = "Submit";
 
 form.appendChild(labelEmail);
 form.appendChild(inputEmail);
+form.appendChild(spanEmail);
 form.appendChild(labelCountry);
 form.appendChild(inputCountry);
+form.appendChild(spanCountry);
 form.appendChild(labelZip);
 form.appendChild(inputZip);
+form.appendChild(spanZip);
 form.appendChild(labelPass);
 form.appendChild(inputPass);
+form.appendChild(spanPass);
 form.appendChild(labelPassConf);
 form.appendChild(inputPassConf);
+form.appendChild(spanPassConf);
 form.appendChild(button);
 body.appendChild(form);
+
+// validity
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+inputEmail.addEventListener("input", (event) => {
+  if (inputEmail.validity.typeMismatch) {
+    inputEmail.setCustomValidity("Enter a valid email address!");
+  } else {
+    inputEmail.setCustomValidity("");
+  }
+});
